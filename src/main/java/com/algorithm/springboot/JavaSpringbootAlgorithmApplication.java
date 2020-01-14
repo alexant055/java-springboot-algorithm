9d2a04dc96766c355c9920a1b1856c215168777b
Antony <ALEXANDER_ANTONY@homedepot.com>
2020-01-13 20:04:36 -0500

    first commit

---
+++ src/main/java/com/algorithm/springboot/JavaSpringbootAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.algorithm.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorithm/springboot/JavaSpringbootAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithm.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
